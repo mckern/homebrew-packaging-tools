88e17e56bab3a5785064f53f99b1bdb6d96f5dab
Ryan McKern <ryan.mckern@puppetlabs.com>
2016-04-09 13:58:57 -0700

    Homebrew style formatting fixups for rpmdevtools
    Adding a description and Homebrew specific Rubocop fixes.

--- rpmdevtools.rb
+++ rpmdevtools.rb
@@ -1,38 +1,36 @@
-require 'formula'
-
 class Rpmdevtools < Formula
-  homepage 'https://fedorahosted.org/rpmdevtools/'
-  url 'https://fedorahosted.org/releases/r/p/rpmdevtools/rpmdevtools-8.6.tar.xz'
-  version '8.6'
-  sha256 '566c6d7a05d80ab9e20414d6aa8df7328578261f3d997686e53da09917f66b38'
+  desc "Many scripts to aid in RPM package development."
+  homepage "https://fedorahosted.org/rpmdevtools/"
+  url "https://fedorahosted.org/releases/r/p/rpmdevtools/rpmdevtools-8.6.tar.xz"
+  sha256 "566c6d7a05d80ab9e20414d6aa8df7328578261f3d997686e53da09917f66b38"
 
-  depends_on 'help2man'
-  depends_on 'rpm4'
+  depends_on "help2man"
+  depends_on "rpm4"
   depends_on :python
 
   patch :DATA
 
   def install
-    args = %W(
+    args = %W[
       --prefix=#{prefix}
       --localstatedir=#{var}
-    )
+    ]
 
-    system './configure', *args
-    system 'make'
+    system "./configure", *args
+    system "make"
 
-    %w(
+    %w[
       rpmdev-bumpspec
       rpmdev-checksig
       rpmdev-rmdevelrpms
       rpmdev-rmdevelrpms.py
       rpmdev-sort
       rpmdev-vercmp
-    ).each do |bin|
-      inreplace bin, '#!/usr/bin/python', '#!/usr/bin/env python'
+    ].each do |bin|
+      inreplace bin, "#!/usr/bin/python", "#!/usr/bin/env python"
     end
 
-    system 'make install'
+    system "make", "install"
   end
 end
 
